06cf5a87dd9094c3c64fede661bf2cd42612e687
John-Kimani <kimwaweru100@gmail.com>
2022-03-07 00:53:36 +0300

    Dev: Creates pitch class defines variables for pitch instance

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -17,3 +18,21 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Pitch(db.Model):
+    '''
+    Class that defines pitch instance made by users
+    Args:
+        Timestamp: To help retrive pitch in order and set as default to keep record of user posting
+        user_id variable references user id(one to many db relationship)
+    Returns:
+        Users timestamp as at the time they created pitch messages
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Pitch {}>'.format(self.body)
+    
