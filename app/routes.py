44c0308e5a52570b57322645f058c4f86c094093
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 15:02:58 +0300

    Dev: Create get and post methods upon validation user is returned to home page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -12,11 +12,15 @@ def index():
     title = "HOME"
     return render_template('index.html', title=title)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     '''
     View function that set display on login page
     '''
     form = LoginForm()
     title = "LogIn"
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title=title, form=form )
