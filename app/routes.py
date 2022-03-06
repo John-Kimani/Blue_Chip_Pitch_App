0dfb9ec6d34863658b562f565f0e6742f88939ef
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 14:13:15 +0300

    Dev: Create Title and implements flask-wtf forms Sets hardcoded secret key to start with

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    title = "HOME"
+    return render_template('index.html', title=title)
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Config(object):
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
