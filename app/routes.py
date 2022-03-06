33a5fa100a0f7e37cc818e989112dac4fbb6038d
John-Kimani <kimwaweru100@gmail.com>
2022-03-06 14:39:25 +0300

    Dev: Creates user login form Login form login

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    '''
+    View function that set display on home page
+    '''
     title = "HOME"
     return render_template('index.html', title=title)
+
+@app.route('/login')
+def login():
+    '''
+    View function that set display on login page
+    '''
+    form = LoginForm()
+    title = "LogIn"
+    return render_template('login.html', title=title, form=form )
