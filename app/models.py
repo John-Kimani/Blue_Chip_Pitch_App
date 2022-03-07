9baad244a8c8e4a12e6feec7c04bcddaba3241fc
John-Kimani <kimwaweru100@gmail.com>
2022-03-07 13:19:51 +0300

    Test: Pitch instance on shell

--- app/models.py
+++ app/models.py
@@ -24,6 +24,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    pitch = db.relationship('Pitch', backref='author', lazy='dynamic')
 
     def __repr__(self):
         '''
@@ -62,8 +63,11 @@ class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #reference user id
 
     def __repr__(self):
+        '''
+        Function to display user pitch
+        '''
         return '<Pitch {}>'.format(self.body)
     
