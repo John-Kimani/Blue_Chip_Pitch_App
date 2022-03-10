33931329af526bf29134a830a2715151b5274234
John-Kimani <kimwaweru100@gmail.com>
2022-03-10 03:00:03 +0300

    Dev: Create 500 error handler

--- app/errors/errors.py
+++ app/errors/errors.py
@@ -1,7 +1,16 @@
 from app.errors import bp
 from flask import render_template
+from app import db
 
 
 @bp.app_errorhandler(404)
 def not_found(error):
     return render_template('errors/404.html'), 404
+
+@bp.app_errorhandler(500)
+def internal_server_error(error):
+    '''
+    Function that handles server error
+    '''
+    db.sessions.rollback()
+    return render_template('errors/500.html'), 500 
